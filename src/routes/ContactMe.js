87ea49b54ef0f1461c0defeb22ebbb8f3b7fa9ec
jschuyl <schuylertjed@gmail.com>
2023-01-18 17:56:38 -0700

    email form works

--- src/routes/ContactMe.js
+++ src/routes/ContactMe.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {useRef} from "react";
+import emailjs from '@emailjs/browser';
 
 export default function ContactMe() {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault(); //prevent refresh
+  
+      emailjs.sendForm('gmail', 'templatePortfolio', e.target, 'Kz1NuGs3oKKPSGook')
+        .then((result) => {
+            console.log(result.text);
+            form.current.reset();
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  
     return (
         <>
-        <form>
-            <label>
-                Email me!
-                <input type="text" name="fname" placeholder="First name"></input>
-                <input type="text" name="lname" placeholder="Last name"></input>
-                <input type="text" name="email" placeholder="Email address"></input>
-                <input type="submit" name="fireAway" value="Send"></input>
-            </label>
-        </form>
+      <form className="container emailForm" ref={form} onSubmit={sendEmail}>
+        <h2>Contact Me!</h2>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
         </>
-    )
-}
+    );
+  };
