81b3eea8621c5a4bc34673cb6706521636327476
jschuyl <schuylertjed@gmail.com>
2023-01-24 21:42:08 -0700

    adding casrousel

--- src/components/PortfolioMain.js
+++ src/components/PortfolioMain.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import Card from './Card'
 //import { Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/carousel/lib/styles'
 //import AboutMe from '../routes/AboutMe';
 
 export default function PortfolioMain() {
 
     return (
         <>
-        <Container>
+        <container className="card__container center">
             <Card 
             img=""
             title="README.generator"
-            describe="Command line operated README generator, with links and everything"/>\
+            describe="Command line operated README generator, with links and everything"/>
             <Card />
             <Card />
             <Card />
             <Card />
             <Card />
-        </Container>
+        </container>
         </>
     );
 }
