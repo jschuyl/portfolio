497b6e32d28b144582be99684f34135e225b2258
jschuyl <schuylertjed@gmail.com>
2023-02-12 16:56:59 -0700

    pictures are sized wrong

--- build/precache-manifest.b2996e48cf30857cd308e23a3c403173.js
+++ build/precache-manifest.b2996e48cf30857cd308e23a3c403173.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "50ee3bfb4c6410f822a2",
-    "url": "/portfolio/static/css/main.8cfd421e.chunk.css"
+    "revision": "36a9fa79eea530f7289e",
+    "url": "/portfolio/static/css/main.53953c60.chunk.css"
   },
   {
-    "revision": "50ee3bfb4c6410f822a2",
+    "revision": "36a9fa79eea530f7289e",
     "url": "/portfolio/static/js/main.91ffe43b.chunk.js"
   },
   {
@@ -24,16 +24,16 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/itJed.b56c8dfd.jpg"
   },
   {
-    "revision": "17629a5dfe0d3c3946cf401e1895f091",
-    "url": "/portfolio/static/media/revicons.17629a5d.ttf"
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portfolio/static/media/revicons.04eb8fc5.woff"
   },
   {
     "revision": "2feb69ccb596730c72920c6ba3e37ef8",
     "url": "/portfolio/static/media/revicons.2feb69cc.eot"
   },
   {
-    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
-    "url": "/portfolio/static/media/revicons.04eb8fc5.woff"
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portfolio/static/media/revicons.17629a5d.ttf"
   },
   {
     "revision": "12a21bd51cbea5a2e403fb79c1ec8201",
@@ -60,7 +60,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/social.91ca953f.jpg"
   },
   {
-    "revision": "b5155e2ede3e97189bd91aebe3611fdc",
+    "revision": "b64c73c33710f65e7a5475bc70a7f94e",
     "url": "/portfolio/index.html"
   }
 ];
