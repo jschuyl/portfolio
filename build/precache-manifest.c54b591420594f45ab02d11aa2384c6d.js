91ca491f8ab1599d978408d580e71d6e9acbc091
jschuyl <schuylertjed@gmail.com>
2023-02-12 11:45:54 -0700

    fixing deployment issues

---
+++ build/precache-manifest.c54b591420594f45ab02d11aa2384c6d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8a4ebcad0d56ed878f",
+    "url": "/portfolio/static/css/main.cf0b61d8.chunk.css"
+  },
+  {
+    "revision": "1f8a4ebcad0d56ed878f",
+    "url": "/portfolio/static/js/main.39845405.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "7d73fda6387e5f67337f",
+    "url": "/portfolio/static/css/2.cb84a128.chunk.css"
+  },
+  {
+    "revision": "7d73fda6387e5f67337f",
+    "url": "/portfolio/static/js/2.1888f53c.chunk.js"
+  },
+  {
+    "revision": "b56c8dfde4c3504296c96d1871ef29fc",
+    "url": "/portfolio/static/media/itJed.b56c8dfd.jpg"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "12a21bd51cbea5a2e403fb79c1ec8201",
+    "url": "/portfolio/static/media/devtek.12a21bd5.jpg"
+  },
+  {
+    "revision": "6771ea16bc87c08b4d54beb7cd7e5ed6",
+    "url": "/portfolio/static/media/employee.6771ea16.jpg"
+  },
+  {
+    "revision": "ad2adce96954029c3da165c32b7c93a8",
+    "url": "/portfolio/static/media/lmay.ad2adce9.jpg"
+  },
+  {
+    "revision": "2ca802195b0315534c1025fb00283423",
+    "url": "/portfolio/static/media/mbb.2ca80219.jpg"
+  },
+  {
+    "revision": "92a5e53e74d1fa1864f21e49e00c956f",
+    "url": "/portfolio/static/media/pwacticejpg.92a5e53e.jpg"
+  },
+  {
+    "revision": "91ca953f73d874e4a1c37210c3d1e749",
+    "url": "/portfolio/static/media/social.91ca953f.jpg"
+  },
+  {
+    "revision": "93e2079f942b3175137f62010f56bdad",
+    "url": "/portfolio/index.html"
+  }
+];
